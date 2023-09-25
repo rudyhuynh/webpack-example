d8dba060a563d7879abf8c2918526cc53bf72205
Rudy Huynh <rudy.huynh286@gmail.com>
2023-09-25 20:47:28 +0700

    end of S2 Babel

--- src/main.js
+++ src/main.js
@@ -1,13 +1,8 @@
 import "./style.css";
-import webpackLogo from "./assets/webpack-logo.svg";
 
 function main() {
   const root = document.getElementById("root");
-  root.textContent = "Modern Javascript App with ";
-
-  const img = document.createElement("img");
-  img.src = webpackLogo;
-  root.append(img);
+  root.textContent = "2^5 = " + 2 ** 5; // Math.pow(2, 5)
 }
 
 main();
--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,7 @@ module.exports = {
     clean: true,
   },
   mode: "development",
+  devtool: "source-map",
   module: {
     rules: [
       {
@@ -18,6 +19,16 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
+      },
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
       },
     ],
   },
