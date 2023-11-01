84bb5545fc8605b84bf09ffcd1579ee2812549b8
Rudy Huynh <rudy.huynh286@gmail.com>
2023-11-01 20:14:46 +0700

    update

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -2,20 +2,20 @@ export const NavigationBar = () => {
   return (
     <nav>
       <a
-        href="/home.html"
-        className={location.pathname === "/home.html" ? "active" : ""}
+        href="home.html"
+        className={location.pathname.endsWith("/home.html") ? "active" : ""}
       >
         Home
       </a>
       <a
-        href="/math.html"
+        href="math.html"
         className={location.pathname === "/math.html" ? "active" : ""}
       >
         Exponentiation
       </a>
       <a
-        href="/about.html"
-        className={location.pathname === "/about.html" ? "active" : ""}
+        href="about.html"
+        className={location.pathname.endsWith("about.html") ? "active" : ""}
       >
         About
       </a>
