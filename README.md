a650943f7531f48698351326b65406efa6037654
Rudy Huynh <rudy.huynh286@gmail.com>
2023-11-11 12:01:53 +0700

    update readme

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ Basic example for Micro Frontend structure using Webpack Module Federation.
 
 ```
 git clone https://github.com/rudyhuynh/webpack-example.git
-git checkout S8-Micro-Frontend
+git checkout S8-Micro-Frontend-Typescript
 npm install
 ```
--- app1/src/App.tsx
+++ app1/src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
 
-export const App = () => {
+type Props = { text?: string };
+
+export const App = ({ text }: Props) => {
   return (
     <div className="text-center">
-      <span className="App1Text">This is App 1</span>
+      <span className="App1Text">This is App 1. {text}</span>
     </div>
   );
 };
